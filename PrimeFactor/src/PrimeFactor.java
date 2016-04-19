b7becb0dc9a1c9cf9a42f7074a36587d93f6a7b0
aronmorris <aron@undo.com>
2016-04-18 22:21:25 -0400

    Next prime factor on user request complete

--- PrimeFactor/src/PrimeFactor.java
+++ PrimeFactor/src/PrimeFactor.java
@@ -19,18 +19,24 @@ public class PrimeFactor {
 			if (n == -1) {
 				break;
 			}
+			
+			//Divide until value is no longer even
 			while (n % 2 == 0) {
 				System.out.printf("%d ", 2);
 				n = n / 2;
 			}
 			
-			for (int i = 3; i < Math.sqrt(n); i += 2) {
+			n = (int) Math.sqrt(n);
+			
+			//Divide by every value until it can't divide by it further if the value is a divisor
+			//While also not using any even values since they've been eliminated
+			for (int i = 3; i < n; i += 2) {
 				while (n % i == 0) {
 					System.out.printf("%d ", i);
 					n = n / i;
 				}
 			}
-			
+			//In case the remaining value is > 2 it's printed
 			if (n > 2) {
 				System.out.printf("%d ", n);
 			}
