a9177ba070de8e819c04d5eff0495824442f2f59
Aron Sigurdsson-Morris <aron@undo.com>
2016-05-01 00:21:29 -0400

    Alarm date scheduling near completion
    Java’s Date/Calendar/SimpleDateFormat is super awkward to work with

--- AlarmClock/src/Alarm.java
+++ AlarmClock/src/Alarm.java
@@ -19,55 +19,30 @@ public class Alarm extends Observer{
 	@Override
 	public void update(String day, String hour, boolean repeats, String msg) {
 		
-		cal.set(Calendar.DAY_OF_WEEK, parseDay(day));
-		parseHour(hour);
+		Date cDate = new Date();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("k");
+		
+		try {
+			cDate = sdf.parse(hour);
+			
+			cal.setTime(cDate);
+			System.out.println(cal.HOUR_OF_DAY);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if (repeats) {
+			timer.scheduleAtFixedRate(new AlarmTask(msg), cDate, TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS));
+		}
+		else {
+			
+		}
 		
 		timer.schedule(new AlarmTask(msg), cal.getTime(), TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS));
 	}
 
-	/**
-	 * Parses a string of form "[int] [AM/PM]" and returns the time from it
-	 * @param hour
-	 * @return
-	 */
-	protected int parseHour(String hour) {
-		int offset = 0;
-		int rHour = 0;
-		
-		if (hour.contains(hour.toUpperCase())) {
-			offset = 12;
-		}
-		
-		rHour = Integer.parseInt(hour.substring(0, hour.indexOf(':')));
-		System.out.println("Hours: " + (rHour + offset));
-		return rHour + offset;
-		
-	}
-	
-	//TODO reformat this so the minute parameter returns validly
-	protected void parseMinute(String hour) {
-		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-		Date date = null;
-		try {
-			date = sdf.parse(hour);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		cal.setTime(date);
-		
-	}
-	
-	protected int parseDay(String day) {
-		int rDay= 0;
-		final String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-		
-		for (int i = 0; i < Calendar.SATURDAY; i++) {
-			if (daysOfWeek[i].equalsIgnoreCase(day)) {
-				rDay = i;
-			}
-		}
-		System.out.println("Days: " + rDay);
-		return rDay;
-	}
 
 }
