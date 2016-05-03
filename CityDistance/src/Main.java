c2f43d04be00ae743f15db30c9de4e29b4ed83dd
Aron Sigurdsson-Morris <aron@undo.com>
2016-05-02 23:00:12 -0400

    Fixed main method to take any location as an argument

--- CityDistance/src/Main.java
+++ CityDistance/src/Main.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		CoordinateCalculator cc = new CoordinateCalculator();
+		Scanner sc = new Scanner(System.in);
 		
-		cc.setFirstCity("Montreal");
+		System.out.println("Enter the names of the two cities you want to find the distance between.");
+		System.out.println("Please separate them with the enter key.");
 		
-		cc.setSecondCity("New York");
+		String city1, city2;
+		
+		city1 = sc.next();
+		
+		city2 = sc.next();
+		
+		CoordinateCalculator cc = new CoordinateCalculator(city1, city2);
+		
+		//cc.setFirstCity("Montreal");
+		
+		//cc.setSecondCity("New York");
 		
 		cc.query();
+		
+		sc.close();
 		
 	}
 	
