d91c70b0bdeed90c4d655d13892e3d172ae8bdef
aronmorris <aron@undo.com>
2016-05-09 20:00:01 -0400

    completed collatz conjecture

---
+++ CollatzConjecture/src/Collatz.java
@@ -0,0 +1,31 @@
+
+public class Collatz {
+
+	public static void main(String[] args) {
+		System.out.printf("Steps to reach 1, not counting the input as a step: %d%n", stepsToReachOne(6));
+	}
+	
+	public static int stepsToReachOne(int startingVal) {
+		int ctr = 0;
+		int n = startingVal;
+		
+		while (startingVal != 1) {
+			if (startingVal <= 1) {
+				break;
+			}
+			ctr++;
+			
+			if (startingVal % 2 == 0) {
+				startingVal /= 2;
+			}
+			
+			else {
+				startingVal = startingVal * 3 + 1;
+			}
+			System.out.println(startingVal);
+		}
+		return ctr;
+		
+	}
+	
+}
