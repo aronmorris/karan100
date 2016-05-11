002b9083412a6e92d81b20aeff85f987a389f4b5
aronmorris <aron@undo.com>
2016-05-10 21:57:16 -0400

    mergesort done

--- MergeAndBubbleSorts/src/MergeAndBubbleSort.java
+++ MergeAndBubbleSorts/src/MergeAndBubbleSort.java
@@ -6,8 +6,11 @@ public class MergeAndBubbleSort {
 	public static void main(String[] args) {
 		int[] toSort = {8, 7, 6, 5, 4, 3, 2, 1};
 		System.out.println(Arrays.toString(bubbleSort(toSort)));
+		System.out.println(Arrays.toString(mergeSort(toSort)));
 	}
 	
+	
+	// <bubbleSort>
 	public static int[] bubbleSort(int[] arr) {
 		
 		int n = arr.length;
@@ -38,6 +41,59 @@ public class MergeAndBubbleSort {
 		
 		return arr;
 	}
+	// </bubbleSort>
 	
+	// <mergeSort>
+	public static int[] mergeSort(int[] arr) {
+		int[] sArr = arr;
+		int[] working = new int[sArr.length]; //working array, holds values only
+		
+		topDownMerge(sArr, 0, sArr.length, working); 
+			
+		return sArr;
+	}
+
+	//merges from the top down recursively
+	private static void topDownMerge(int[] A, int begin, int end,
+			int[] B) {
+		if (end - begin < 2) { //These two are ordered
+			return; //base case
+		}
+		
+		int middle = (end + begin) / 2; //split the array in half
+		
+		topDownMerge(A, begin, middle, B); //merge from start to middle
+		topDownMerge(A, middle, end, B); //end to middle
+		//merge the two arrays
+		//This is reached only when every sub-array has a length of 1
+		merge(A, begin, middle, end, B);
+		copyArray(B, begin, end, A); //copies the working and final arrays back together, overriding A
+		
+	}
+
+	//Copies the arrays together, overriding A with the sorted B as many times as there are smaller arrays to combine up to the final A, the sorted version of the input
+	private static void copyArray(int[] B, int begin, int end, int[] A) {
+		for (int k = begin; k < end; k++) {
+			A[k] = B[k];
+		}
+	}
+	
+	//Called when every array sublist has been divided down into 1 element each
+	private static void merge(int[] A, int begin, int middle, int end, int[] B) {
+		int i = begin, j = middle;
+		
+		for (int k = begin; k < end; k++) {
+			if (i < middle && (j >= end || A[i] <= A[j])) { //if A[start] less than A[middle], then we're good
+				B[k] = A[i];
+				i += 1;
+			}
+			else {
+				B[k] = A[j]; //otherwise the middle value of A is the correct one and gets added to the sorted B
+				j += 1;
+			}
+		}
+		
+	}
+	// </mergeSort>
 	
 }
