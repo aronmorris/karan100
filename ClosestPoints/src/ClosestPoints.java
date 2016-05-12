86bc7886e166054c49d50ec5c0c3713db5e543d6
aronmorris <aron@undo.com>
2016-05-11 21:07:05 -0400

    Completed closest pair problem!

--- ClosestPoints/src/ClosestPoints.java
+++ ClosestPoints/src/ClosestPoints.java
@@ -1,22 +1,24 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ClosestPoints {
 
 	public static void main(String[] args) {
-		Tuple[] listOfPoints = {new Tuple(1,3),new Tuple(3,4),new Tuple(2,2),new Tuple(5,3)};
-		ArrayList<Tuple> ans = closestPair(sortX(listOfPoints), sortY(listOfPoints));
+		Tuple[] listOfPoints = {new Tuple(1,3),new Tuple(3,4),new Tuple(2,2), new Tuple(3, 5),new Tuple(5,3)};
+		HashMap<Double, ArrayList<Tuple>> ans = closestPair(sortX(listOfPoints), sortY(listOfPoints));
 		System.out.printf(ans.toString());
 	}
 	
 	/**
-	 * Identifies closest points in O(nlogn) time
+	 * Identifies closest points in O(nlogn) time, implements https://www.rosettacode.org/wiki/Closest-pair_problem algorithm
 	 * @param xPoints sorted X ascending order
 	 * @param yPoints sorted Y ascending order
 	 * @return
 	 */
-	public static ArrayList<Tuple> closestPair(Tuple[] xPoints, Tuple[] yPoints) {
+	public static HashMap<Double, ArrayList<Tuple>> closestPair(Tuple[] xPoints, Tuple[] yPoints) {
 		if (xPoints.length <= 3) {
 			return bruteClosestPair(xPoints);
 		}
@@ -27,7 +29,7 @@ public class ClosestPoints {
 			Tuple xMid = xPoints[xPoints.length / 2];
 			
 			//populate arraylists
-			for (int i = 0; i <= xPoints.length; i++) {
+			for (int i = 0; i < xPoints.length; i++) {
 				if (i < xPoints.length / 2) {
 					xLeft.add(xPoints[i]);
 				}
@@ -40,7 +42,7 @@ public class ClosestPoints {
 			ArrayList<Tuple> yRight = new ArrayList<Tuple>();
 			
 			//populate arraylists
-			for (int i = 0; i <= yPoints.length; i++) {
+			for (int i = 0; i < yPoints.length; i++) {
 				if (yPoints[i].X() <= xMid.X()) {
 					yLeft.add(yPoints[i]);
 				}
@@ -49,16 +51,60 @@ public class ClosestPoints {
 				}
 			}
 		
+			HashMap<Double, ArrayList<Tuple>> left = new HashMap<Double, ArrayList<Tuple>>();
+			HashMap<Double, ArrayList<Tuple>> right = new HashMap<Double, ArrayList<Tuple>>();
+			Tuple[] leftX = new Tuple[xLeft.size()], 
+					leftY = new Tuple[yLeft.size()],
+					rightX = new Tuple[xRight.size()],
+					rightY = new Tuple[yRight.size()];
+			//left-right recursion implemented right down to the bottom of the nth layer of hell, key "AAAAAA"
+			left = closestPair(xLeft.toArray(leftX), yLeft.toArray(leftY));
+			right = closestPair(xRight.toArray(rightX), yRight.toArray(rightY));
 			
+			HashMap<Double, ArrayList<Tuple>> dpMin = new HashMap<Double, ArrayList<Tuple>>();
 			
+			dpMin = right; //min is legit just right array
+			
+			double dMin = (double)left.keySet().toArray()[0];
+			//int nS = 0;
+			
+			if (dMin < (double)right.keySet().toArray()[0]) {
+				dpMin = left;
+			}
+			
+			ArrayList<Tuple> ySmall = new ArrayList<Tuple>();
+			for (int i = 0; i < yPoints.length; i++) {
+				if (Math.abs(xMid.X() - yPoints[1].X()) < dMin) {
+					ySmall.add(yPoints[i]);
+				}
+			}
+			
+			HashMap<Double, ArrayList<Tuple>> closest = dpMin;
+			//actual comparison sorting of the smallest elements
+			for (int i = 1; i <= ySmall.size() - 1; i++) {
+				int k = i + 1;
+				while (k <= ySmall.size() && (ySmall.get(k).Y() - ySmall.get(i).Y()) < dMin) {
+					if (Math.abs(ySmall.get(k).Y() - ySmall.get(i).Y()) < (double) dpMin.keySet().toArray()[0]) {
+						ArrayList<Tuple> val = new ArrayList<Tuple>();
+						val.add(ySmall.get(k));
+						val.add(ySmall.get(i));
+						HashMap<Double, ArrayList<Tuple>> temp = new HashMap<Double, ArrayList<Tuple>>();
+						temp.put(Math.abs((double)ySmall.get(k).Y() - ySmall.get(i).Y()), val);
+						closest = temp;
+					}
+					k += 1;
+				}
+			}
+			return closest;
 		}
-		return null;
 	}
 	
 	//brute forcing it
-	public static ArrayList<Tuple> bruteClosestPair(Tuple[] points) {
+	public static HashMap<Double, ArrayList<Tuple>> bruteClosestPair(Tuple[] points) {
 		
 		ArrayList<Tuple> minPts = new ArrayList<Tuple>();
+		
+		double minDistance = Double.MAX_VALUE;
 		
 		if (points.length < 2) { //only one element, it'd be the closest to itself
 			for (int i = 0; i < points.length; i++) {
@@ -66,7 +112,7 @@ public class ClosestPoints {
 			}
 		}
 		else {
-			double minDistance = Math.sqrt(Math.abs(  Math.pow((points[1].X() - points[2].X()), 2) + Math.pow((points[1].Y() - points[2].Y()), 2)  ));
+			minDistance = Math.sqrt(Math.abs(  Math.pow((points[0].X() - points[1].X()), 2) + Math.pow((points[0].Y() - points[1].Y()), 2)  ));
 			minPts.add(points[0]);
 			minPts.add(points[1]);
 			
@@ -81,7 +127,11 @@ public class ClosestPoints {
 				}
 			}
 		}
-		return minPts;
+		
+		HashMap<Double, ArrayList<Tuple>> retM = new HashMap<Double, ArrayList<Tuple>>();
+		retM.put(minDistance, minPts);
+		
+		return retM;
 		
 	}
 	//sorts in ascending order of X
