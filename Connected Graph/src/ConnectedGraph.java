cf5bae0139b01f5946c3b18850b86b7cb6118023
Aron Sigurdsson-Morris <aron@undo.com>
2016-05-20 23:26:22 -0400

    began connected graph (depth-first implementation)

---
+++ Connected Graph/src/ConnectedGraph.java
@@ -0,0 +1,17 @@
+import com.aronmorris.graph.Graph;
+
+
+public class ConnectedGraph {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public boolean isConnected(Graph graph) {
+		
+		//TODO implement depth-first search
+		
+		return false;
+	}
+	
+}
