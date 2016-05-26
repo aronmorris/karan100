956ab6f7633da6a9003992433b21e021066be5c6
Aron Sigurdsson-Morris <aron@undo.com>
2016-05-25 20:03:17 -0400

    refactor away from IO for now
    It’s tougher to serialize a singleton and I’m not sure of my reasons
    for making it a singleton originally

--- Inverted Index/src/IndexMap.java
+++ Inverted Index/src/IndexMap.java
@@ -1,41 +1,53 @@
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class IndexMap implements Serializable {
+public class IndexMap {
 
-	private static final long serialVersionUID = 123L;
-
-	private static IndexMap instance = null;
+	private static HashMap<String, ArrayList<String>> index; 
+	private static HashMap<String, Integer> occurrences;
 	
-	protected IndexMap() {
+	public IndexMap() {
+		
+		index = new HashMap<String, ArrayList<String>>();
+		occurrences = new HashMap<String, Integer>();
+		
 		//defeats instantiation
-		try
+		/*try
 	      {
-	         FileInputStream fileIn = new FileInputStream("/src/tmp/index.ser");
+	         FileInputStream fileIn = new FileInputStream("./src/tmp/index.ser");
 	         ObjectInputStream in = new ObjectInputStream(fileIn);
 	         instance = (IndexMap) in.readObject();
 	         in.close();
 	         fileIn.close();
-	      }catch(IOException i)
-	      {
+	      } catch(FileNotFoundException f) {
+	    	  f.printStackTrace();
+	    	  new File("./src/tmp/").mkdirs();
+	    	  instance = new IndexMap(-1);
+	    	  
+	      } catch(IOException i) {
 	         i.printStackTrace();
 	         return;
-	      }catch(ClassNotFoundException c)
-	      {
+	      } catch(ClassNotFoundException c) {
 	         System.out.println("IndexMap class not found.");
 	         c.printStackTrace();
 	         return;
 	      }
+	      */
 	}
-	
+
+	/*
 	public static boolean end() {
 		try {
 	         FileOutputStream fileOut =
-	         new FileOutputStream("/src/tmp/index.ser");
+	         new FileOutputStream("./src/tmp/index.ser");
 	         ObjectOutputStream out = new ObjectOutputStream(fileOut);
 	         out.writeObject(instance);
 	         out.close();
@@ -48,20 +60,34 @@ public class IndexMap implements Serializable {
 	      }
 
 	}
-	
+	*/
+	/*
 	public static IndexMap getIndexMap() {
 		if (instance == null) {
 			instance = new IndexMap();
 		}
 		return instance;
 	}
-	
-	//TODO fix these here
-	public static boolean addToken() {
-		return false;
+	*/
+	public static Integer get(String token) {
+		return occurrences.get(token);
 	}
 	
-	public static boolean removeToken() {
+	//TODO fix these here
+	public static boolean addToken(String token) {
+		
+		if (occurrences.containsKey(token)) {
+			int count = occurrences.get(token);
+			occurrences.put(token, count);
+		} else {
+			occurrences.put(token, 1); //add to occurrence index	
+		}
+		
+		return true;
+		
+	}
+	
+	public static boolean removeToken(String token) {
 		return false;
 	}
 	
--- Inverted Index/src/InvertedIndex.java
+++ Inverted Index/src/InvertedIndex.java
@@ -1,28 +1,74 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class InvertedIndex {
 
 	private static IndexMap indexMap;
 	
 	public static void main(String[] args) {
+		File file = new File("./src/tmp/Text.txt");
+		
+		//begin();
+		
+		//System.out.println(IndexMap.get("hello"));
+		
+		indexMap = new IndexMap();
+		
+		index(file);
+		
+		System.out.println(IndexMap.get("hello"));
+		
+		//end();
 		
 	}
 	
-	public static void begin() {
+	/*public static void begin() {
 		indexMap = IndexMap.getIndexMap();
 	}
-	
-	public static void end() {
-		indexMap.end();
+	*/
+	public static void index(File file) {
+		try (BufferedReader br = new BufferedReader(new FileReader(file))){
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+			
+			while (line != null) {
+				sb.append(line);
+				sb.append(System.lineSeparator());
+				line = br.readLine();
+			}
+			
+			String built = sb.toString();
+			
+			index(tokenize(built), file);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
 	}
-	
+	/*
+	public static void end() {
+		IndexMap.end();
+	}
+	*/
 	public static String[] tokenize(String input) {
+		input = input.replaceAll("[^a-zA-Z0-9'\\s]+"," ");
 		String[] retStr = input.split("\\s+"); //splits by any and all whitespace
 		return retStr;
 	}
 	
-	public static void index(String[] strArr, File doc, File index) {
-		
+	private static void index(String[] strArr, File srcDoc) {
+		for (String s : strArr) {
+			s = s.toLowerCase();
+			IndexMap.addToken(s);
+		}
 	}
 	
 }
