fbbc139e43b9b7746bb8cabc3ac80904b371b7ff
Aron Sigurdsson-Morris <aron@undo.com>
2016-05-29 19:58:59 -0400

    More UI work

--- Inverted Index/src/IndexUIWindow.java
+++ Inverted Index/src/IndexUIWindow.java
@@ -1,24 +1,70 @@
 import java.awt.EventQueue;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.List;
+import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.Button;
+import java.awt.Component;
+
+import javax.swing.Box;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.SwingConstants;
+
+import net.miginfocom.swing.MigLayout;
+
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JList;
+import javax.swing.border.BevelBorder;
 
 
 public class IndexUIWindow {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
+			
+			IndexMap index = new IndexMap();
+			
 			public void run() {
 				try {
 					IndexUIWindow window = new IndexUIWindow();
 					window.frame.setVisible(true);
+			
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+				
+				
+				
 			}
 		});
 	}
@@ -37,6 +83,51 @@ public class IndexUIWindow {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JButton btnSearch = new JButton("Search");
+		btnSearch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JList list = new JList();
+		
+		list.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED, Color.WHITE, Color.GRAY));
+		
+		JButton btnChooseFile = new JButton("Choose File");
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, 306, GroupLayout.PREFERRED_SIZE)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(6)
+							.addComponent(list, GroupLayout.PREFERRED_SIZE, 294, GroupLayout.PREFERRED_SIZE)))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(btnChooseFile, 0, 0, Short.MAX_VALUE)
+						.addComponent(btnSearch, GroupLayout.DEFAULT_SIZE, 112, Short.MAX_VALUE))
+					.addGap(20))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnSearch)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnChooseFile)
+						.addComponent(list, GroupLayout.PREFERRED_SIZE, 199, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(37, Short.MAX_VALUE))
+		);
+		frame.getContentPane().setLayout(groupLayout);
+		
 	}
-
 }
