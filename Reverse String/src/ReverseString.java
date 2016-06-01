351eb6b533d2c77b6717f88afa4295bc38685149
Aron Sigurdsson-Morris <aron@undo.com>
2016-05-31 21:42:58 -0400

    Reverse string done

---
+++ Reverse String/src/ReverseString.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String tor = sc.nextLine().toString();
+		
+		System.out.println(new StringBuilder(tor).reverse());
+		
+	}
+	
+}
