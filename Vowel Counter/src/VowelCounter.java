e49f7bfc4f448dc0981b7411f3aa608ea6dac05a
Aron Sigurdsson-Morris <aron@undo.com>
2016-06-03 00:04:09 -0400

    Finalized: counts on per-vowel basis

--- Vowel Counter/src/VowelCounter.java
+++ Vowel Counter/src/VowelCounter.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 
@@ -6,18 +7,25 @@ public class VowelCounter {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		
-		String vowels = "[aeiouy]";
-		String in = sc.nextLine();
+		String vowels = "[AEIOUY]";
+		HashMap<String, Integer> counts = new HashMap<String, Integer>();
 		int ctr = 0;
 		
-		for (String c : in.split("")) {
+		String in = sc.nextLine();
+		
+		for (String c : in.toUpperCase().split("")) {
 			if (vowels.contains(c)) {
-				ctr++;
+				ctr = (counts.get(c) == null ? 0 : counts.get(c));
+				counts.put(c, ctr + 1);
+				ctr = 0;
 			}
 		}
+	
+		for (String k : counts.keySet()) {
+			System.out.printf("Instances of \"%s\": %d%n", k, counts.get(k));
+		}
 		
-		System.out.println(ctr);
+		sc.close();
 		
 	}
 	
