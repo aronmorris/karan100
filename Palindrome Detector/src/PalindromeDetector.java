71e86ef82f4a932ffa9a04fa50da480b6467d835
Aron Sigurdsson-Morris <aron@undo.com>
2016-06-05 17:20:22 -0400

    completed palindrome detector

--- Palindrome Detector/src/PalindromeDetector.java
+++ Palindrome Detector/src/PalindromeDetector.java
@@ -2,6 +2,20 @@
 public class PalindromeDetector {
 
 	public static void main(String[] args) {
+		System.out.println(isPalindrome("Hello"));
+		System.out.println(isPalindrome("HellolleH"));
+	}
+	
+	public static boolean isPalindrome(String str) {
+		String[] split = str.split("");
+		
+		//i iterates upwards, j iterates downwards
+		for (int i = 0, j = split.length - 1; i < split.length && j > 0; i++, j--) { 
+			if (!split[i].equals(split[j])) {
+				return false;
+			}
+		}
+		return true;
 		
 	}
 	
