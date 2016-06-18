98588b23ab7cc3fc16f1b8002f5566d98c6d2e60
Aron Sigurdsson-Morris <aron@undo.com>
2016-06-17 23:46:06 -0400

    Caesar Cipher implemented

---
+++ Cipher/src/CaesarCipher.java
@@ -0,0 +1,24 @@
+
+public class CaesarCipher {
+
+	private static char[] message;
+	
+	public static String encrypt(String msg, char shift) {
+		message = msg.toCharArray();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (char c : message) {
+			if (c + shift > 'z') {
+				c -= shift;
+			}
+			else {
+				c += shift;
+			}
+			sb.append(c);
+		}
+		
+		return sb.toString();
+	}
+	
+}
---
+++ Cipher/src/Cipher.java
@@ -0,0 +1,8 @@
+
+public class Cipher {
+
+	public static void main(String[] args) {
+		System.out.println(CaesarCipher.encrypt("ABCDE", (char) 1));
+	}
+	
+}
