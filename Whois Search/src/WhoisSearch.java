a1ed4e1ae11f785d04b98508b3f342591cc0c10d
Aron Sigurdsson-Morris <aron@undo.com>
2016-06-30 17:40:33 -0400

    Completed whois on internic server

--- Whois Search/src/WhoisSearch.java
+++ Whois Search/src/WhoisSearch.java
@@ -1,4 +1,37 @@
+import java.io.IOException;
+import java.net.SocketException;
+
+import org.apache.commons.net.whois.WhoisClient;
+
 
 public class WhoisSearch {
 
+	public static void main(String[] args) {
+		
+		WhoisClient wClient = new WhoisClient();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		try {
+			
+			wClient.connect(WhoisClient.DEFAULT_HOST);
+	
+			String data = wClient.query("=" + args[0]);
+			
+			sb.append(data);
+			
+			wClient.disconnect();
+			
+		} catch (SocketException e) {
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(sb.toString());
+		
+	}
+	
 }
