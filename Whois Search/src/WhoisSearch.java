72f0e972d8a2f35d99e2cf51a076e46e6c21699a
aronmorris <aron@undo.com>
2016-06-30 21:39:23 -0400

    Whois working for internic

--- Whois Search/src/WhoisSearch.java
+++ Whois Search/src/WhoisSearch.java
@@ -1,4 +1,40 @@
+import java.io.IOException;
+import java.net.SocketException;
+
+import org.apache.commons.net.whois.WhoisClient;
+
 
 public class WhoisSearch {
 
+	public static void main(String[] args) {
+		
+		System.out.println(whois(args[0]));
+		
+	}
+	
+	public static String whois(String domain) {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		WhoisClient whois = new WhoisClient();
+		
+		try {
+			whois.connect(WhoisClient.DEFAULT_HOST);
+			
+			String dat = whois.query("=" + domain);
+			
+			sb.append(dat);
+			
+			whois.disconnect();
+
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return sb.toString();
+		
+	}
+	
 }
