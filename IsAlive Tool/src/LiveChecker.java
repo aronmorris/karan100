f91309f23c4d4bec7601d12b8d916208020c04c2
Aron Sigurdsson-Morris <aron@undo.com>
2016-07-05 22:14:04 -0400

    Completed site alive checker

--- IsAlive Tool/src/LiveChecker.java
+++ IsAlive Tool/src/LiveChecker.java
@@ -1,17 +1,61 @@
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 
 public class LiveChecker {
 
+	static class CheckRunnable {
+		
+		private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+		
+		private String url;
+		
+		private int timeout;
+		
+		public CheckRunnable(String url, int timeout) {
+			this.url = url;
+			this.timeout = timeout;
+		}
+		
+		public void checkEveryMinute() {
+			
+			final Runnable checker = new Runnable() {
+				public void run() { testConnection(url, timeout); }
+			};
+			//schedules check on url every minute
+			final ScheduledFuture<?> checkHandler =
+					scheduler.scheduleAtFixedRate(checker, 1, 1, TimeUnit.MINUTES);
+			//stop checking after a day by default
+			scheduler.schedule(new Runnable() {
+				public void run() {checkHandler.cancel(true); }
+			}, 60 * 60, TimeUnit.MINUTES); 
+		
+			
+		}
+	}
+	
 	static final int ECHO_PORT = 7;
 	
 	public static void main(String[] args) {
 		
-		String url = args[0].contains("http://") ? args[0] : "http://" + args[0]; //append protocol to ensure httpurl works
+		//protocol amended to url if missing
+		String url = args[0].contains("http://") ? args[0] : "http://" + args[0];
 		
 		int timeout = Integer.parseInt(args[1]);
+		
+		CheckRunnable checker = new CheckRunnable(url, timeout);
+		
+		checker.checkEveryMinute();
+		
+		
+	}
+	
+	public static boolean testConnection(String url, int timeout) {
 		
 		try {
 			
@@ -26,13 +70,25 @@ public class LiveChecker {
 			int response = connection.getResponseCode(); //200-399 ok
 			
 			if (200 <= response && response <= 399) {
+				
 				System.out.println(response + " ok");
+				
+				return true;
 			}
 			
 		} catch (IOException e) {
-			System.out.println("HTTP code not in range 200-399 KO");
-			e.printStackTrace();
+			
+			System.out.println("HTTP code not in range 200-399 KO");	
+			
+			System.out.println("URL not responding!");
+			
+			return false;
+			
+			
+			
 		}
+		
+		return false;
 		
 	}
 	
