89a65ee84d9c8b4eb0c03e588d58f6dd688a652d
aronmorris <aron@undo.com>
2016-08-22 22:45:38 -0400

    Progress rendering handled for progress bar

--- Download Project Bar/src/ProgressRenderer.java
+++ Download Project Bar/src/ProgressRenderer.java
@@ -1,4 +1,30 @@
+import java.awt.Component;
 
-public class ProgressRenderer {
+import javax.swing.JProgressBar;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
 
+
+public class ProgressRenderer extends JProgressBar implements TableCellRenderer {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -588998258059638338L;
+
+	public ProgressRenderer(int min, int max) {
+		super(min, max); //passes off args to JProgressBar
+	}
+	
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value,
+			boolean isSelect, boolean hasFocus, int row, int column) {
+		
+		setValue((int) ((Float) value).floatValue()); //set percentage complete value
+		
+		return null;
+	}
+
+	
+	
 }
