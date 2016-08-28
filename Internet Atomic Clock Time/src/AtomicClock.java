c2cf56fd3feefa27eec60cb10b39e6ac932bd4f5
aronmorris <aron@undo.com>
2016-08-27 23:49:58 -0400

    started atomic clock fetcher, skipping whiteboard until Play Framework is installed for java-built web apps

---
+++ Internet Atomic Clock Time/src/AtomicClock.java
@@ -0,0 +1,7 @@
+
+public class AtomicClock {
+
+	//TODO use the hourmin1 span on http://www.timeanddate.com/time/international-atomic-time.html to get the current UTC time
+	//at the moment of retrieval. Use Jsoup, this is its territory
+	
+}
