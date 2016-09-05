79855857fda1bdbb370c287bae9c1dd07a2df181
aronmorris <aron@undo.com>
2016-09-04 23:42:39 -0400

    Work on eCardGraphic itself, to be displayed and interacted with thru Spark

--- eCard Generator Site/src/eCardGraphic.java
+++ eCard Generator Site/src/eCardGraphic.java
@@ -1,6 +1,28 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
 
 public class eCardGraphic {
 
+	final BufferedImage image;
 	
+	String imgText;
+	
+	public eCardGraphic(String text, BufferedImage image) {
+		this.image = image;
+		
+		Graphics g = image.getGraphics();
+		
+		g.setFont(g.getFont().deriveFont(30f));
+		
+		g.drawString(text, 100, 100);
+		
+		g.dispose();
+		
+		ImageIO.write(this.image, "png", new File("test.png"));
+		
+	}
 	
 }
