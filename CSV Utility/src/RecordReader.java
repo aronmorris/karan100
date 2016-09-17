912993023dab08abd967090edf489ea1586d5f87
aronmorris <aron@undo.com>
2016-09-16 22:21:37 -0400

    Started CSV sorting utility

---
+++ CSV Utility/src/RecordReader.java
@@ -0,0 +1,11 @@
+
+/**
+ * Reads CSV/Excel files into Records, which are the object manipulated for sorting
+ * 
+ * Acts as a wrapper for the Apache Commons CSV API along with the rest of this project
+ * @author Aron
+ *
+ */
+public class RecordReader {
+
+}
---
+++ CSV Utility/src/RecordWriter.java
@@ -0,0 +1,9 @@
+
+/**
+ * Writes Records to new files or overwrites old ones
+ * @author Aron
+ *
+ */
+public class RecordWriter {
+
+}
