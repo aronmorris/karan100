d166487ba09fa8690eccaf35bf9fc37cedb7d730
aronmorris <aron@undo.com>
2016-09-23 23:45:02 -0400

    UI skeleton in place, todo: add methods controlling zip/unzip operation and recognizing zip archives

--- Zipper/src/ZipperUI.java
+++ Zipper/src/ZipperUI.java
@@ -8,6 +8,11 @@ import javax.swing.JFileChooser;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import java.awt.CardLayout;
+import javax.swing.JSplitPane;
+import javax.swing.border.MatteBorder;
+import java.awt.Color;
 
 /*
  * UI is used to find and select one or multiple files without relying on
@@ -49,6 +54,25 @@ public class ZipperUI {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		SpringLayout springLayout = new SpringLayout();
 		frame.getContentPane().setLayout(springLayout);
+		
+		JPanel panel = new JPanel();
+		springLayout.putConstraint(SpringLayout.EAST, panel, -10, SpringLayout.EAST, frame.getContentPane());
+		panel.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		springLayout.putConstraint(SpringLayout.NORTH, panel, 10, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, panel, -10, SpringLayout.SOUTH, frame.getContentPane());
+		frame.getContentPane().add(panel);
+		
+		JPanel panel_1 = new JPanel();
+		springLayout.putConstraint(SpringLayout.WEST, panel, 164, SpringLayout.EAST, panel_1);
+		springLayout.putConstraint(SpringLayout.NORTH, panel_1, 0, SpringLayout.NORTH, panel);
+		springLayout.putConstraint(SpringLayout.WEST, panel_1, 10, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, panel_1, 0, SpringLayout.SOUTH, panel);
+		
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2);
+		springLayout.putConstraint(SpringLayout.EAST, panel_1, -374, SpringLayout.EAST, frame.getContentPane());
+		panel_1.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		frame.getContentPane().add(panel_1);
 		
 		int widthPos = frame.WIDTH;
 		
