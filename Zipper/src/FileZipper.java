e8256618574e9a5a4403da40453e20e6258cb43f
aronmorris <aron@undo.com>
2016-09-29 19:02:52 -0400

    Refactored zip method to own class & compress multiple files to one zip archive

--- Zipper/src/FileZipper.java
+++ Zipper/src/FileZipper.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -24,19 +25,41 @@ public class FileZipper {
 	
 	}
 	
-	private void zip(File[] f) {
+	private void zip(File[] f, String destinationFileName) {
 		
 		byte[] buffer = new byte[2048];
 		
 		ZipEntry[] entries = new ZipEntry[f.length];
 		
 		for (int i = 0; i < f.length; i++) {
-			entries[i] = new ZipEntry(f[i].getAbsolutePath() + f[i].getName()); //populate the ZipEntry array
+			entries[i] = new ZipEntry(f[i].getAbsolutePath()); //populate the ZipEntry array
 		}
 		
-		try (FileOutputStream fileOut = new FileOutputStream(entry.getName() + ".zip")) { //create output stream for the new zipped file
+		try (FileOutputStream fileOut = new FileOutputStream(destinationFileName + (destinationFileName.contains(".zip") ? "" : ".zip"))) { //create output stream for the new zipped file, add .zip extension if it isn't there already
 			
 			ZipOutputStream zipOut = new ZipOutputStream(fileOut); //zip stream that feeds into the output
+			
+			FileInputStream input;
+			
+			for (File fileToCompress : f) {
+				
+				System.out.println("Now compressing " + fileToCompress.getName());
+				
+				input = new FileInputStream(fileToCompress.getAbsolutePath());
+				
+				int length;
+				
+				while ((length = input.read(buffer)) > 0) {
+					zipOut.write(buffer, 0, length); //write length bytes into the file
+				}
+							
+				zipOut.closeEntry(); //close the current entry, prepare stream to write the next entry
+				
+				input.close();
+				
+			}
+			
+			zipOut.close();
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
