2a795a51d4faddf1e5b702c87555c86b38ae1759
aronmorris <aron@undo.com>
2016-09-30 20:49:36 -0400

    refactoring zip/unzip methods start to their own classes

--- Zipper/src/FileZipper.java
+++ Zipper/src/FileZipper.java
@@ -1,4 +1,9 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /*
  * Takes one or more files as a constructor argument, which it then processes
@@ -12,6 +17,34 @@ public class FileZipper {
 	private File[] files;
 	
 	public FileZipper(File... files) {
+		
+		for (File f : files) {
+			
+		}
+	
+	}
+	
+	private void zip(File[] f) {
+		
+		byte[] buffer = new byte[2048];
+		
+		ZipEntry[] entries = new ZipEntry[f.length];
+		
+		for (int i = 0; i < f.length; i++) {
+			entries[i] = new ZipEntry(f[i].getAbsolutePath() + f[i].getName()); //populate the ZipEntry array
+		}
+		
+		try (FileOutputStream fileOut = new FileOutputStream(entry.getName() + ".zip")) { //create output stream for the new zipped file
+			
+			ZipOutputStream zipOut = new ZipOutputStream(fileOut); //zip stream that feeds into the output
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+			
+		}
 		
 	}
 	
