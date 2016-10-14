c12ebf6e7b130591559310f6b3eb9e8773238020
aronmorris <aron@undo.com>
2016-10-13 23:40:01 -0400

    Groundwork for IO and writing to list

---
+++ Code Snippet Manager/src/SnippetIO.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+
+public class SnippetIO {
+
+	//location of the file storing the snippets
+	private final String FILE_LOC = File.separator + "resources" + File.separator + "snippets.txt";
+	
+	/*
+	 * TODO
+	 * This class will read the snippets.txt file and write its contents to the list model, with each line going into the list, separated by newline characters
+	 * It will also read from the list to write to the file. Appended writing isn't needed since the file only stores the current contents of the list, and if an
+	 * item is deleted from the list, it's clearly no longer desired to be kept
+	 */
+	
+}
--- Code Snippet Manager/src/SnippetWriter.java
+++ Code Snippet Manager/src/SnippetWriter.java
@@ -1,6 +1,39 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SnippetWriter {
+import javax.swing.DefaultListModel;
+import javax.swing.JTextField;
 
+
+public class SnippetWriter implements ActionListener {
+
+	private final JTextField textField;
+	private final DefaultListModel<String> listModel;
+	
+	public SnippetWriter(JTextField field, DefaultListModel<String> list) {
+		textField = field;
+		listModel = list;
+	}
+	
+	private String retrieveText() {
+		
+		return textField.getText();
+	}
+
+	//adds the text written in the text field to the list if the entry wasn't null or empty
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+
+		String textRetrieved = retrieveText();
+		
+		if (textRetrieved.equals(null) || textRetrieved.equals("")) {
+			return;
+		}
+		
+		listModel.addElement(retrieveText());
+		
+	}
+	
 	
 	
 }
