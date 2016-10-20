7b84054ce32b14fe2b61c5d48ea764f14ca5d9f0
aronmorris <aron@undo.com>
2016-10-19 23:35:07 -0400

    Caesar cipher in security section complete

--- Caesar Cipher/src/CaesarCipher.java
+++ Caesar Cipher/src/CaesarCipher.java
@@ -9,23 +9,23 @@ public class CaesarCipher {
 	//converts the string to a char array, then rotates each char by *key* spaces, adding them to the final "encrypted" result
 	//before returning the resultant string
 	
-	public String encrypt(String msg, int shift){
+	public String encrypt(String msg, int key){
 	    String s = "";
 	    int len = msg.length();
 	    for(int x = 0; x < len; x++){
-	        char c = (char)(msg.charAt(x) + shift);
+	        char c = (char)(msg.charAt(x) + key);
 	        if (c > 'z')
-	            s += (char)(msg.charAt(x) - (26-shift));
+	            s += (char)(msg.charAt(x) - (26-key));
 	        else
-	            s += (char)(msg.charAt(x) + shift);
+	            s += (char)(msg.charAt(x) + key);
 	    }
 	    return s;
 	}
 	
 	//simply inverts the key, runs the encryption again, reversing prior rotation, then re-inverts the key to its original value
 	//doesn't work if the string's been encrypted multiple times
-	public String unencrypt() {
-		return null;
+	public String decrypt(String encrypted, int key) {
+		return encrypt(encrypted, -key);
 		
 	}
 	
@@ -33,7 +33,9 @@ public class CaesarCipher {
 		
 		CaesarCipher cipher = new CaesarCipher();
 		
-		System.out.println(cipher.encrypt("ABC abc", 1));
+		System.out.println(cipher.encrypt("ABC abc", 13));
+		
+		System.out.println(cipher.decrypt(cipher.encrypt("ABC abc", 13), 13));
 		
 	}
 	
