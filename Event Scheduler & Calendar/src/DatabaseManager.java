73ae3e9ae4458ef9b30e7745085ecbb70d40bf12
aronmorris <aron@undo.com>
2016-10-20 00:47:08 -0400

    testing sql, added connector to build path

--- Event Scheduler & Calendar/src/DatabaseManager.java
+++ Event Scheduler & Calendar/src/DatabaseManager.java
@@ -1,4 +1,28 @@
+import java.sql.*;
 
 public class DatabaseManager {
 
+	public static void viewTable(Connection con, String dbName) {
+		
+		String query = "select *" +
+					   "from " + dbName;
+		
+		 try (Statement stmt = con.createStatement()) {
+		        ResultSet rs = stmt.executeQuery(query);
+		        while (rs.next()) {
+		            String coffeeName = rs.getString("COF_NAME");
+		            int supplierID = rs.getInt("SUP_ID");
+		            float price = rs.getFloat("PRICE");
+		            int sales = rs.getInt("SALES");
+		            int total = rs.getInt("TOTAL");
+		            System.out.println(coffeeName + "\t" + supplierID +
+		                               "\t" + price + "\t" + sales +
+		                               "\t" + total);
+		        }
+		    } catch (SQLException e ) {
+		        e.printStackTrace();
+		}
+		
+	}
+	
 }
