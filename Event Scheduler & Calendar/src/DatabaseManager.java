272eba2ede5eab1b1f495ecf5e1a4c4d0b98b6f5
aronmorris <aron@undo.com>
2016-10-22 23:58:28 -0400

    Reformatting db manager class for ease of use

--- Event Scheduler & Calendar/src/DatabaseManager.java
+++ Event Scheduler & Calendar/src/DatabaseManager.java
@@ -1,45 +1,60 @@
 import java.sql.*;
+import java.time.LocalDate;
 
 public class DatabaseManager {
-
-	public static void main(String[] args) {
+	
+	private static String mysqlUsername;
+	private static String mysqlPassword;
+	
+	//takes the defined username and password, connecting to the MSQL server internally defined for testing purposes
+	private Connection connect() {
 		
 		String driverName = "org.gjt.mm.mysql.Driver";
+		String serverName = "localhost:3306";
+		String dbName = "java_test";
+		String url = "jdbc:mysql://" + serverName + "/" + dbName + "?autoReconnect=true&useSSL=false";
+		
+		Connection con = null;
 		
 		try {
 				
 			Class.forName(driverName);
-	
-			String serverName = "localhost:3306";
-			
-			String dbName = "java_test";
-			
-			String url = "jdbc:mysql://" + serverName + "/" + dbName + "?autoReconnect=true&useSSL=false";
-			
-			
-			String user = "--user--";
-			
-			String pw = "--PW--";
-			
-			Connection con;
-			
-			con = DriverManager.getConnection(url, user, pw);
+				
+			con = DriverManager.getConnection(url, mysqlUsername, mysqlPassword);
 		
-			
-			Statement stmt = con.createStatement();
-			
-			String query = "SELECT id_date, event_scheduled FROM " + dbName + ".event_scheduler";
-			
-			ResultSet rs = stmt.executeQuery(query);
-			
-			while (rs.next()) {
-				System.out.println(rs.getString("id_date") + " | " + rs.getString("event_scheduled"));
-			}
-	
-		} catch(Exception e) {
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		
+		return con;
 	}
+	
+	//inserts new event at a date into the database
+	//date is YYYY-MM-DD format, one event per day until reformatted here and in the db
+	private void insertToDatabase(String formattedDate, String eventDescription) {
+		Connection con = connect();
+		
+		
+	}
+	//formats user-provided data to the correct format to be inserted into the DB
+	public static void addEventAtDate(LocalDate date, String eventDescription) {
+		
+		String driverName = "org.gjt.mm.mysql.Driver";
+		
+		
+	}
+	/*
+	Statement stmt = con.createStatement();
+	
+	String query = "SELECT id_date, event_scheduled FROM " + dbName + ".event_scheduler";
+	
+	ResultSet rs = stmt.executeQuery(query);
+	
+	while (rs.next()) {
+		System.out.println(rs.getString("id_date") + " | " + rs.getString("event_scheduled"));
+	}
+	*/
 	
 }
