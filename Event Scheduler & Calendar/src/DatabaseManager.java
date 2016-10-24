e1cc3a6f8f15cb076599ab3109adca8c2fa7bffb
aronmorris <aron@undo.com>
2016-10-23 22:31:40 -0400

    Insert/select working from db in dbManager

--- Event Scheduler & Calendar/src/DatabaseManager.java
+++ Event Scheduler & Calendar/src/DatabaseManager.java
@@ -1,17 +1,35 @@
 import java.sql.*;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class DatabaseManager {
 	
 	private static String mysqlUsername;
 	private static String mysqlPassword;
 	
+	private static String dbName = "java_test";
+	private static String table = ".event_scheduler";
+	
+	public static void main(String[] args) {
+	
+		DatabaseManager.setCredentials("root", "There once was a genie with a 10 foot weenie");
+		
+		DatabaseManager.insertToDatabase("2016-12-10", "Event at the place");
+		
+		
+		
+	}
+	
+	public static void setCredentials(String user, String pw) {
+		mysqlUsername = user;
+		mysqlPassword = pw;
+	}
+	
 	//takes the defined username and password, connecting to the MSQL server internally defined for testing purposes
-	private Connection connect() {
+	private static Connection connect() {
 		
 		String driverName = "org.gjt.mm.mysql.Driver";
 		String serverName = "localhost:3306";
-		String dbName = "java_test";
 		String url = "jdbc:mysql://" + serverName + "/" + dbName + "?autoReconnect=true&useSSL=false";
 		
 		Connection con = null;
@@ -33,8 +51,24 @@ public class DatabaseManager {
 	
 	//inserts new event at a date into the database
 	//date is YYYY-MM-DD format, one event per day until reformatted here and in the db
-	private void insertToDatabase(String formattedDate, String eventDescription) {
+	private static void insertToDatabase(String formattedDate, String eventDescription) {
 		Connection con = connect();
+		
+		String sqlInsert = "INSERT INTO " + dbName + table + " VALUES "
+				+ "(\"" + formattedDate + "\", \"" + eventDescription + "\");";
+						 
+		try {
+			Statement stmt = con.createStatement();
+			
+			stmt.executeUpdate(sqlInsert);
+			
+			read(stmt);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 		
 	}
@@ -43,7 +77,16 @@ public class DatabaseManager {
 		
 		String driverName = "org.gjt.mm.mysql.Driver";
 		
+	}
+	
+	private static void read(Statement stmt) throws SQLException { 
+		String query = "SELECT id_date, event_scheduled FROM " + dbName + ".event_scheduler";
 		
+		ResultSet rs = stmt.executeQuery(query);
+		
+		while (rs.next()) {
+			System.out.println(rs.getString("id_date") + " | " + rs.getString("event_scheduled"));
+		}
 	}
 	/*
 	Statement stmt = con.createStatement();
