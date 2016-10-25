cc472207aed238a18eb1fb807e9d65d1e951df91
aronmorris <aron@undo.com>
2016-10-24 22:59:30 -0400

    SQL working but inefficiently

--- Event Scheduler & Calendar/src/DatabaseManager.java
+++ Event Scheduler & Calendar/src/DatabaseManager.java
@@ -1,5 +1,6 @@
 import java.sql.*;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class DatabaseManager {
@@ -12,11 +13,47 @@ public class DatabaseManager {
 	
 	public static void main(String[] args) {
 	
-		DatabaseManager.setCredentials("root", "There once was a genie with a 10 foot weenie");
+		//pw & user sanitized out
+		DatabaseManager.setCredentials("--", "--");
 		
-		DatabaseManager.insertToDatabase("2016-12-10", "Event at the place");
+		DatabaseManager.addEventAtDate(LocalDate.now(), "Testing mysql queries, should probably buy a book or something for this");
 		
 		
+		
+	}
+	
+	//Unsure of how to handle key replacement vs insertion with one query command, using this
+	//in the meanwhile though it's inefficient
+	//Updating once possible
+	public static void addEventAtDate(LocalDate date, String eventDescription) {
+		
+		//converts date to ISO-8601 to compare to the 
+		date.format(DateTimeFormatter.BASIC_ISO_DATE);
+		
+		String eventDate = date.toString();
+		
+		try {
+			Statement stmt = connect().createStatement();
+			
+			String getExistingDates = "SELECT id_date "
+									+ "FROM " + dbName + table;
+			
+			ResultSet rs = stmt.executeQuery(getExistingDates);
+			
+			while (rs.next()) {
+				if (rs.getString("id_date").equals(eventDate)) {
+					replaceInDatabase(eventDate, eventDescription);
+					return;
+				}
+			}
+			
+			insertToDatabase(eventDate, eventDescription);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
@@ -26,7 +63,7 @@ public class DatabaseManager {
 	}
 	
 	//takes the defined username and password, connecting to the MSQL server internally defined for testing purposes
-	private static Connection connect() {
+	private static Connection connect() throws ClassNotFoundException, SQLException {
 		
 		String driverName = "org.gjt.mm.mysql.Driver";
 		String serverName = "localhost:3306";
@@ -34,48 +71,40 @@ public class DatabaseManager {
 		
 		Connection con = null;
 		
-		try {
-				
-			Class.forName(driverName);
-				
-			con = DriverManager.getConnection(url, mysqlUsername, mysqlPassword);
-		
-		} catch(ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+			
+		Class.forName(driverName);
+			
+		con = DriverManager.getConnection(url, mysqlUsername, mysqlPassword);
+	
 		
 		return con;
 	}
 	
 	//inserts new event at a date into the database
 	//date is YYYY-MM-DD format, one event per day until reformatted here and in the db
-	private static void insertToDatabase(String formattedDate, String eventDescription) {
+	private static void insertToDatabase(String formattedDate, String eventDescription) throws SQLException, ClassNotFoundException {
 		Connection con = connect();
 		
 		String sqlInsert = "INSERT INTO " + dbName + table + " VALUES "
 				+ "(\"" + formattedDate + "\", \"" + eventDescription + "\");";
 						 
-		try {
-			Statement stmt = con.createStatement();
-			
-			stmt.executeUpdate(sqlInsert);
-			
-			read(stmt);
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
+		Statement stmt = con.createStatement();
+		
+		stmt.executeUpdate(sqlInsert);
+		
+		read(stmt);
+			
 		
 		
 	}
-	//formats user-provided data to the correct format to be inserted into the DB
-	public static void addEventAtDate(LocalDate date, String eventDescription) {
+	
+	private static void replaceInDatabase(String formattedDate, String eventDescription) throws ClassNotFoundException, SQLException {
 		
-		String driverName = "org.gjt.mm.mysql.Driver";
+		Connection con = connect();
+		
+		String sqlInsert = "REPLACE INTO " + dbName + table + " VALUES "
+				+ "(\"" + formattedDate + "\", \"" + eventDescription + "\");";
 		
 	}
 	
