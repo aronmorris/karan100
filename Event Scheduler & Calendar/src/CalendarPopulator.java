ce6903e942364813072eef4ca72ae8e11ac42014
aronmorris <aron@undo.com>
2016-10-30 23:50:34 -0400

    Added JDatePicker external lib to offload trouble of creating calendar

--- Event Scheduler & Calendar/src/CalendarPopulator.java
+++ Event Scheduler & Calendar/src/CalendarPopulator.java
@@ -1,4 +1,11 @@
+import java.util.Calendar;
+import java.util.Properties;
+
 import javax.swing.JPanel;
+
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
 
 /**
  * CalendarPopulator is responsible for refreshing the calendar frames in the 
@@ -12,8 +19,25 @@ public class CalendarPopulator {
 
 	private final JPanel calendarPanel;
 	
+	private JDatePickerImpl picker;
+	private UtilDateModel model;
+	
 	public CalendarPopulator(JPanel calendarPanel) {
 		this.calendarPanel = calendarPanel;
+		
+		model = new UtilDateModel();
+		
+		model.setDate(Calendar.YEAR, Calendar.MONTH, Calendar.DAY_OF_YEAR);
+		
+		Properties prop = new Properties();
+		
+		prop.put("text.today", "Today");
+		prop.put("text.month", "Month");
+		prop.put("text.year", "Year");
+		JDatePanelImpl datePanel = new JDatePanelImpl(model, prop);
+		
+		
 	}
+	
 	
 }
