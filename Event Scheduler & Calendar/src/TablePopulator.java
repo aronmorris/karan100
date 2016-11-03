23deddfc2c8fa7dd31e6ad07ccb5e4d9269b7557
aronmorris <aron@undo.com>
2016-11-03 02:05:37 -0400

    Attempting to add listeners for table selection

--- Event Scheduler & Calendar/src/TablePopulator.java
+++ Event Scheduler & Calendar/src/TablePopulator.java
@@ -1,4 +1,43 @@
+import java.sql.ResultSet;
+import java.time.LocalDate;
 
-public class TablePopulator {
+import javax.swing.JTable;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
 
+
+public class TablePopulator implements TableModelListener, ListSelectionListener {
+
+	private final JTable table;
+	
+	private int selectedRow;
+	
+	public TablePopulator(JTable table) {
+		
+		this.table = table;
+		
+	}
+
+	@Override
+	public void tableChanged(TableModelEvent tme) {
+		
+		System.out.println("Table changed");
+		
+		//ResultSet res = DatabaseManager.getEventsAtDate(date);
+		
+		
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent lse) {
+		
+		System.out.println("List changed");
+		
+		selectedRow = table.getSelectedRow();
+		
+	}
+	
 }
