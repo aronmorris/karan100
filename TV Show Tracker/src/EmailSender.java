00963c7a1bcfc80349e253a62e85bacd57a5c492
aronmorris <aron@undo.com>
2016-11-11 02:36:05 -0500

    minor update of adding Jaunt API, JavaMail API, & starting in on email sender while outlining remainder

--- TV Show Tracker/src/EmailSender.java
+++ TV Show Tracker/src/EmailSender.java
@@ -1,4 +1,24 @@
+import javax.mail.Session;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 
 public class EmailSender {
 
+	
+	public static void main(String[] args) throws NamingException {
+		
+		EmailSender send = new EmailSender();
+		
+	}
+	public EmailSender() throws NamingException {
+		
+		InitialContext ctx = new InitialContext();
+		
+		Session session = (Session) ctx.lookup("usersMailSession");
+		
+		System.out.println(session.toString());
+		
+	}
+	
 }
